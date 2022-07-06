606e09aa5ad88bd46c98ac2eacd1abbe06ec685b
Ivan <86818646+Trejoivan@users.noreply.github.com>
2022-07-05 21:43:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance'); 
-const inputDuration = document.querySelector('.form__input--duration');
+const inputDuration = document.querySelector('.form__input--duration'); 
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation'); 
 
