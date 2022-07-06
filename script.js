2742d625e545dbd64052a1e8c435843509660211
Ivan <86818646+Trejoivan@users.noreply.github.com>
2022-07-05 21:43:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance'); 
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const inputElevation = document.querySelector('.form__input--elevation'); 
 
 class Workout {
   date = new Date();
